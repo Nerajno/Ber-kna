de71cea88e5a20c94ffaa3a0fb92463cac31b87e
Nerando Johnson <nerajno@gmail.com>
2024-01-13 17:41:35 -0500

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const buttonPress = ( symbol: string ) => {
+    console.log( symbol);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h1>Calculator @ FCC</h1>
+        <div id='calculator'>
+          <div id='display' style={{ textAlign: 'right' }}>
+            <div id='answer'></div>
+            <div id='expression'></div>
+            <button id="clear" onClick={() => buttonPress('clear')} className='light-gray'></button>
+            <button id="negative" onClick={() => buttonPress('negative')} className='light-gray'></button>
+            <button id="percentage" onClick={() => buttonPress('percentage')} className='light-gray'></button>
+            <button id="divide" onClick={() => buttonPress("/")} className='light-gray'></button>
+            <button id="seven" onClick={() => buttonPress('7')} className='light-gray'></button>
+            <button id="eight" onClick={() => buttonPress('8')} className='light-gray'></button>
+            <button id="nine" onClick={() => buttonPress('9')} className='light-gray'></button>
+            <button id="multiply" onClick={() => buttonPress('*')} className='light-gray'></button>
+            <button id="four" onClick={() => buttonPress('4')} className='light-gray'></button>
+            <button id="five" onClick={() => buttonPress('5')} className='light-gray'></button>
+            <button id="six" onClick={() => buttonPress('6')} className='light-gray'></button>
+            <button id="subtract" onClick={() => buttonPress('subtract')} className='light-gray'></button>
+            <button id="one" onClick={() => buttonPress('1')} className='light-gray'></button>
+            <button id="two" onClick={() => buttonPress('2')} className='light-gray'></button>
+            <button id="three" onClick={() => buttonPress('3')} className='light-gray'></button>
+            <button id="add" onClick={() => buttonPress('+')} className='light-gray'></button>
+            <button id="zero" onClick={() => buttonPress('0')} className='light-gray'></button>
+            <button id="decimal" onClick={() => buttonPress('.')} className='light-gray'></button>
+            <button id="equals" onClick={() => buttonPress('=')} className='light-gray'></button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
