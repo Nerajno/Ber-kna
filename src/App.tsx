6fb043dc1adfa6c3eb81a87689be92e53641ed44
Nerando Johnson <nerajno@gmail.com>
2024-01-18 13:18:38 -0500

    Completed js v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,100 @@
 import { useState } from 'react';
-import './App.css';
 
-function App() {
+export function App() {
+  const [expression, setExpression] = useState('');
+  const [answer, setAnswer] = useState('');
+  const et = expression.trim();
+
+  const isOperator = ( symbol: string ) => {
+    switch ( symbol ) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  const calculate = () => {
+      // if the last char is an operator, do not calculate
+     if( isOperator(et.charAt(et.length - 1)) ) return;
+      // clean the expression so that two operators in a row uses the last operator
+      // 5 * - + 5 = 10
+
+     const parts = et.split(" ");
+     const newParts = [];
+
+     for (let i = parts.length - 1; i >= 0; i--) {
+      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i -1])){
+        newParts.unshift(parts[i]);
+        let j = 0;
+        let k = i - 1;
+        while( isOperator(parts[k])){
+          k --;
+          j++;
+        }
+        i -= j;
+      } else {
+        newParts.unshift(parts[i]);
+      }
+    }
+    const newExpression = newParts.join(" ");
+    if(isOperator(newExpression.charAt(0))){
+      setAnswer(eval(answer + newExpression) as string);
+    }else{
+      setAnswer(eval(newExpression) as string);
+    }
+    setExpression("");
+  }
 
   const buttonPress = ( symbol: string ) => {
-    console.log( symbol);
+    //console.log( symbol);
+    if ( symbol === 'clear' ) {
+      setExpression('');
+      setAnswer('0');
+    } else if ( symbol === 'negative' ) {
+      if( answer === '') return;
+      setAnswer(
+        answer.toString().charAt(0) === '-' ? answer.slice(1) : '-' + answer
+      )
+    } else if ( symbol === 'percentage' ) {
+      if( answer === '') return;
+      setAnswer((parseFloat(answer.toString()) / 100).toString());
+    } else if (isOperator(symbol)){
+      setExpression( et + " " + symbol + " ");
+    } else if ( symbol === '=' ) {
+      calculate();
+    }else if ( symbol === '0'){
+      if(expression.charAt(0) !=="0"){
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === '.'){
+      setExpression(expression + symbol );
+      // split by operators and get last number
+      const lstNum = expression.split(/[-+/*]/g).pop();
+      if(!lstNum) return;
+      if( lstNum?.includes('.')) return;;
+      setExpression(expression + symbol)
+    } else {
+      if( expression.charAt(0) === "0"){
+        setExpression(expression.slice(1) + symbol);
+      } else {
+        setExpression(expression + symbol);
+      }
+    }
   }
+
+
   return (
     <>
       <div className='container'>
         <h1>Calculator Application</h1>
         <div id='calculator'>
           <div id='display' style={{ textAlign: 'right' }}>
-            <div id='answer'>{}</div>
-            <div id='expression'>{}</div>
+            <div id='answer'>0</div>
+            <div id='expression'></div>
           </div>
           <button
             id='clear'
@@ -149,5 +230,3 @@ function App() {
     </>
   );
 }
-
-export default App;
